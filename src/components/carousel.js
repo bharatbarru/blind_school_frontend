1851c5941907668c3d032af59a10c620fd3642fc
bharat <bharat.barru57@gmail.com>
2024-10-13 21:59:46 +0530

    changes made

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -30,6 +30,7 @@ export default function SimpleSlider() {
           <img
             src={src}
             style={{ width: '100%', height: '550px' }} 
+            alt="banner images"
           />
         </Box>
       ))}
